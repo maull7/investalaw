0e2735891ec6821b574e21193678e634976850e9
maull7 <rmgaming601@gmail.com>
2026-06-18 15:50:48 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'create'])->name('login');
     Route::post('/login', [LoginController::class, 'store']);
 });
-
+//
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 

da685b929aa3d9d0ce94f5ad1d921f04f941816d
maull7 <rmgaming601@gmail.com>
2026-06-18 15:47:06 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,11 @@ Route::get('/', function () {
     return view('index');
 });
 
-//guest
 Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'create'])->name('login');
     Route::post('/login', [LoginController::class, 'store']);
 });
 
-//auth
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 
